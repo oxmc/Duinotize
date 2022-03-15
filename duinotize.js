aee716a1a04ba60fb64a548cb8da4a1f430e0d98
oxmc <67136658+oxmc@users.noreply.github.com>
2022-03-15 15:48:53 -0700

    Update duinotize.js

--- duinotize.js
+++ duinotize.js
@@ -38,7 +38,7 @@ function Duinotize(opts) {
     let socketWorker = new Worker("https://oxmc.github.io/Duinotize/main.js");
     socketWorker.postMessage('Start,' + opts.username + "," + opts.rigid + "," + wallet_id + "," + opts.difficulty + "," + workerVer + "," + opts.hasher);
     workerVer++;
-    if (typeof opts.onstart != 'undefined' || opts.onstart != null) {
+    if (typeof opts.onstart != 'undefined' && opts.onstart != null) {
       opts.onstart;
     };
   };
